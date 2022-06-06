4dfb47725bb47781fde7f13bc752fcab4c53c9fc
Salih Yılmaz <salih_yil@live.com>
2022-06-06 15:42:16 +0300

    AboutPage

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
+import Pokeball from "../../assets/pokeball.png";
+import PokeballColor from "../../assets/pokeball-color.png";
+
+import { pokemonDataRequest } from "../../store/pokemonData/slice";
+import "././styles.css";
 
 const AboutPage = () => {
   const { pokeName } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { results } = useSelector((state) => state.pokeData);
 
   const handleBack = () => {
     navigate(-1);
   };
 
+  useEffect(() => {
+    dispatch(pokemonDataRequest(pokeName));
+  }, [dispatch, pokeName]);
+
   return (
-    <div>
-      <div>{pokeName}</div>
-      <button onClick={handleBack}>Back</button>
-    </div>
+    <>
+      <div>
+        <button onClick={handleBack}>Back</button>
+
+        {results.map((result, i) => {
+          const typeName = result.types[0].type.name;
+
+          return (
+            <div className={`card-item ${typeName}`} key={i}>
+              <a href={result.name}>
+                <div className={`card-top ${typeName}`}>
+                  <img
+                    className="card-image"
+                    src={result.sprites.other.dream_world.front_default}
+                    alt={result.typeName}
+                  />
+
+                  <img className="pokeball" src={Pokeball} alt="pokeball" />
+                  <img
+                    className="pokeball-color"
+                    src={PokeballColor}
+                    alt="PokeballColor"
+                    width="150"
+                    height="150"
+                  />
+                </div>
+                <div className="card-bottom">
+                  <small>#0{result.id}</small>
+                  <h3>{result.name}</h3>
+                  <div>
+                    <div>
+                      <small>Type: {typeName}</small>
+                    </div>
+                    <div>
+                      <small>Weight: {result.weight}</small>
+                    </div>
+                    <div>
+                      <small>Height: {result.height}</small>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
