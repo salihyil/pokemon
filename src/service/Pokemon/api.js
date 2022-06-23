7126ed31cdae7047e773351130d6092c1ab77f68
Salih Yılmaz <salih_yil@live.com>
2022-06-24 00:55:52 +0300

    api.js

--- src/service/Pokemon/api.js
+++ src/service/Pokemon/api.js
@@ -3,21 +3,20 @@ import axiosInstance, { axiosGet } from "../axios";
 import API_ROUTES from "./apiRoutes";
 
 const pokemonDataAsync = async (pokemonName) => {
-  const pokemonsDataResponse = await axiosInstance
-    .get(`${API_ROUTES.pokemon}/${pokemonName}`)
-    .then((response) => response.data);
+  const pokemonsDataResponse = await axiosInstance.get(
+    `${API_ROUTES.pokemon}/${pokemonName}`
+  );
 
-  return pokemonsDataResponse;
+  return pokemonsDataResponse.data;
 };
 
 const pokemonAllDataAsync = async () => {
-  const pokemonsAllData = await axiosInstance
-    .get(`${API_ROUTES.pokemon}`)
-    .then((response) => response.data);
-  return pokemonsAllData;
+  const pokemonsAllData = await axiosInstance.get(`${API_ROUTES.pokemon}`);
+
+  return pokemonsAllData.data;
 };
 
-const pokemonAsync = async (results) => {
+const pokemonAsync = (results) => {
   const responseData = results.map(async (result) => {
     const pokemonDataResponse = await axiosGet(result.url);
 
